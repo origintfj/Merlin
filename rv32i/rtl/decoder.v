ae9f65e99427aafe7f491a2fea2453518f7edf11
Tom Stanway-Mayers <Tom Stanway-Mayers>
2017-06-18 21:44:17 -0700

    Removed duplicate definitions from decoder

--- rv32i/rtl/decoder.v
+++ rv32i/rtl/decoder.v
@@ -1,18 +1,5 @@
 `include "riscv_defs.v"
 
-//--------------------------------------------------------------
-// decoder definitions
-//--------------------------------------------------------------
-`define ALUOP_FUNCT3_ADDSUB     3'b000
-`define ALUOP_FUNCT3_SLL        3'b001
-`define ALUOP_FUNCT3_SLT        3'b010
-`define ALUOP_FUNCT3_SLTU       3'b011
-`define ALUOP_FUNCT3_XOR        3'b100
-`define ALUOP_FUNCT3_SRLSRA     3'b101
-`define ALUOP_FUNCT3_OR         3'b110
-`define ALUOP_FUNCT3_AND        3'b111
-//--------------------------------------------------------------
-
 module decoder
     #(
         parameter C_XLEN = 32
