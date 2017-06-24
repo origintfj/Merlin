3fbcd836b8616ada34c6c3788d6125ebd64bb373
Tom Stanway-Mayers <Tom Stanway-Mayers>
2017-06-24 15:38:27 -0700

    ex_stage and cs_registers
     * csr writes now happen on an execute boundary
     * exceptions gate commits

--- rv32i/rtl/cs_registers.v
+++ rv32i/rtl/cs_registers.v
@@ -8,16 +8,20 @@ module cs_registers // TODO
         input  wire               clk_en_i,
         input  wire               resetb_i,
         // read/write interface
-        input  wire               access_i,
-        input  wire        [11:0] addr_i,
-        input  wire  [C_XLEN-1:0] data_i,
-        output wire  [C_XLEN-1:0] data_o,
+        input  wire               rd_i,
+        input  wire        [11:0] rd_addr_i,
+        output wire  [C_XLEN-1:0] rd_data_o,
+        input  wire               wr_i, // the write will be ignored if it triggers an exception
+        input  wire        [11:0] wr_addr_i,
+        input  wire  [C_XLEN-1:0] wr_data_i,
         // static i/o
-        output wire               illegal_access_o,
+        output wire               illegal_rd_o,
+        output wire               illegal_wr_o,
         output wire         [1:0] hpl_o
     );
 
-    assign data_o = { C_XLEN {1'b0} };
-    assign illegal_access_o = 1'b0;
+    assign rd_data_o = { C_XLEN {1'b0} };
+    assign illegal_rd_o = 1'b0;
+    assign illegal_wr_o = 1'b0;
     assign hpl_o = 2'b0;
 endmodule
--- rv32i/rtl/ex_stage.v
+++ rv32i/rtl/ex_stage.v
@@ -1,7 +1,5 @@
-// TODO csr writes neet to happen at the execute/commit boundary so
-// exceptions don't result in half done csr instructions (i.e. csr writes
-// without the read)
-// TODO can branches also cause miss-aligned instruction fetch exceptions?
+// TODO can branches also cause miss-aligned instruction fetch exceptions? or
+// only jumps
 //
 
 `include "riscv_defs.v"
@@ -64,12 +62,16 @@ module ex_stage
     //--------------------------------------------------------------
 
     // instruction specific exceptions
-    wire                ex_maif;
-    reg                 ex_load_x0;
+    wire                commit_gating_exception;
+    wire                excp_ferr;
+    wire                excp_uerr;
+    wire                excp_maif;
+    reg                 excp_load_x0;
+    wire                excp_csr_illegal_access;
     // ex stage qualifier logic
     wire                ex_valid;
-    wire                ex_commit;
-    wire                ex_cancel;
+    wire                execute;
+    wire                execute_commit;
     reg  [`SOFID_RANGE] sofid_q;
     reg                 sofid_run;
     // delay stage
@@ -81,12 +83,15 @@ module ex_stage
     reg                 lq_wr_q;
     reg                 sq_wr_q;
     reg                 regd_wr_q;
-    reg                 csr_access_q;
+    reg                 ids_csr_access_q;
+    reg          [11:0] ids_csr_addr_q;
+    reg    [C_XLEN-1:0] ids_csr_wr_data_q;
     reg                 link_q;
     reg    [C_XLEN-1:0] pc_inc_q;
     reg    [C_XLEN-1:0] regs2_data_q;
     reg           [4:0] regd_addr_q;
     reg           [2:0] funct3_q;
+    reg                 csr_illegal_rd_q;
     // ex stage stall controller
     wire                ex_stage_en;
     reg                 exs_stall;
@@ -97,28 +102,31 @@ module ex_stage
     wire   [C_XLEN-1:0] alu_data_out;
     wire                alu_cmp_out;
     // cs registers
-    wire                csr_access;
+    wire                csr_rd;
+    wire                csr_wr;
     wire   [C_XLEN-1:0] csr_data_out;
-    wire                csr_illegal_access;
+    wire                csr_illegal_rd;
+    wire                csr_illegal_wr;
 
     //--------------------------------------------------------------
 
     // interface assignments
     assign ids_stall_o          = exs_stall;
-    assign ids_regd_cncl_load_o = ex_stage_en & ex_cancel & lq_wr_q;
-    assign ids_regd_wr_o        = ex_stage_en & ex_commit & regd_wr_q;
+    assign ids_regd_cncl_load_o = ex_stage_en & ~execute_commit & ids_valid_q & lq_wr_q;
+    assign ids_regd_wr_o        = ex_stage_en &  execute_commit & regd_wr_q;
     assign ids_regd_addr_o      = regd_addr_q;
     //
-    assign hvec_ferr_o      = ex_stage_en & ex_commit & ids_ins_ferr_q;
-    assign hvec_uerr_o      = ex_stage_en & ex_commit & ids_ins_uerr_q;
-    assign hvec_maif_o      = ex_stage_en & ex_commit & ids_jump_q & ex_maif; // TODO
-    assign hvec_ldx0_o      = ex_stage_en & ex_commit & ex_load_x0;
-    assign hvec_ilgl_o      = ex_stage_en & ex_commit & csr_illegal_access;
-    assign hvec_jump_o      = ex_stage_en & ex_commit & ids_jump_q;
+    assign hvec_ferr_o      = ex_stage_en & excp_ferr;
+    assign hvec_uerr_o      = ex_stage_en & excp_uerr;
+    assign hvec_maif_o      = ex_stage_en & excp_maif;
+    assign hvec_ldx0_o      = ex_stage_en & excp_load_x0;
+    assign hvec_ilgl_o      = ex_stage_en & excp_csr_illegal_access;
+        //
+    assign hvec_jump_o      = ex_stage_en & execute_commit & ids_jump_q;
     assign hvec_jump_addr_o = alu_data_out;
     //
-    assign lsq_lq_wr_o      = ex_stage_en & ex_commit & lq_wr_q;
-    assign lsq_sq_wr_o      = ex_stage_en & ex_commit & sq_wr_q;
+    assign lsq_lq_wr_o      = ex_stage_en & execute_commit & lq_wr_q;
+    assign lsq_sq_wr_o      = ex_stage_en & execute_commit & sq_wr_q;
     assign lsq_funct3_o     = funct3_q;
     assign lsq_regd_addr_o  = regd_addr_q;
     assign lsq_regs2_data_o = regs2_data_q;
@@ -127,21 +135,30 @@ module ex_stage
 
     // instruction specific exceptions
     //
-    assign ex_maif = |(alu_data_out[1:0]); // TODO this will change for compressed instructions
+    assign commit_gating_exception = excp_ferr |
+                                     excp_uerr |
+                                     excp_maif |
+                                     excp_csr_illegal_access;
     //
-    always @ (*) begin
-        ex_load_x0 = 1'b0;
-        if (lq_wr_q && regd_addr_q == 5'b0) begin
-            ex_load_x0 = 1'b1;
+    assign excp_ferr = execute & ids_ins_ferr_q;
+    assign excp_uerr = execute & ids_ins_uerr_q;
+    assign excp_maif = execute & ids_jump_q & (|(alu_data_out[1:0])); // TODO this will change for compressed instructions
+    //
+    always @ (*) begin // ex_load_x0 is not a commit gating exception
+        excp_load_x0 = 1'b0;
+        if (execute && lq_wr_q && regd_addr_q == 5'b0) begin
+            excp_load_x0 = 1'b1;
         end
     end
+    //
+    assign excp_csr_illegal_access = execute & (csr_illegal_rd_q | csr_illegal_wr);
 
 
     // ex stage qualifier logic
     //
-    assign ex_valid  = ids_valid_q & (alu_cmp_out | ~ids_cond_q);
-    assign ex_commit = ex_valid &  sofid_run;
-    assign ex_cancel = ex_valid & ~sofid_run;
+    assign ex_valid       = ids_valid_q & (alu_cmp_out | ~ids_cond_q);
+    assign execute        = ex_valid & sofid_run;
+    assign execute_commit = execute & ~commit_gating_exception;
     //
     always @ (posedge clk_i or negedge resetb_i)
     begin
@@ -190,12 +207,15 @@ module ex_stage
                     `ZONE_REGFILE : regd_wr_q <= 1'b1;
                 endcase
                 //
-                csr_access_q <= ids_csr_access_i;
-                link_q       <= ids_link_i;
-                pc_inc_q     <= ids_pc_i + 4; // TODO for compressed instructions this may change
-                regs2_data_q <= ids_regs2_data_i;
-                regd_addr_q  <= ids_regd_addr_i;
-                funct3_q     <= ids_funct3_i;
+                ids_csr_access_q  <= ids_csr_access_i;
+                ids_csr_addr_q    <= ids_csr_addr_i;
+                ids_csr_wr_data_q <= ids_csr_wr_data_i;
+                link_q            <= ids_link_i;
+                pc_inc_q          <= ids_pc_i + 4; // TODO for compressed instructions this may change
+                regs2_data_q      <= ids_regs2_data_i;
+                regd_addr_q       <= ids_regd_addr_i;
+                funct3_q          <= ids_funct3_i;
+                csr_illegal_rd_q  <= csr_illegal_rd;
             end
         end
     end
@@ -208,7 +228,7 @@ module ex_stage
     always @ (*)
     begin
         exs_stall = 1'b0;
-        if (ex_commit) begin
+        if (execute_commit) begin
             if (lsq_full_i & (lq_wr_q | sq_wr_q)) begin
                 exs_stall = 1'b1;
             end
@@ -220,7 +240,7 @@ module ex_stage
     //
     always @ (*)
     begin
-        if (csr_access_q) begin
+        if (ids_csr_access_q) begin
             ids_regd_data_o = csr_data_out;
         end else begin
             ids_regd_data_o = alu_pcinc_mux_out;
@@ -265,7 +285,8 @@ module ex_stage
 
     // cs registers
     //
-    assign csr_access = ids_csr_access_i & ids_valid_i;
+    assign csr_rd = ids_csr_access_i;// & ex_stage_en & ids_valid_i;
+    assign csr_wr = ids_csr_access_q & ex_stage_en & execute;
     //
     cs_registers
         #(
@@ -276,12 +297,15 @@ module ex_stage
             .clk_en_i         (clk_en_i & ex_stage_en),
             .resetb_i         (resetb_i),
             // read/write interface
-            .access_i         (csr_access),
-            .addr_i           (ids_csr_addr_i),
-            .data_i           (ids_csr_wr_data_i),
-            .data_o           (csr_data_out),
+            .rd_i             (csr_rd),
+            .rd_addr_i        (ids_csr_addr_i),
+            .rd_data_o        (csr_data_out),
+            .wr_i             (csr_wr), // the write will be ignored if it triggers an exception
+            .wr_addr_i        (ids_csr_addr_q),
+            .wr_data_i        (ids_csr_wr_data_q),
             // static i/o
-            .illegal_access_o (csr_illegal_access),
+            .illegal_rd_o     (csr_illegal_rd), // this o/p is valid in the same cycle as the request
+            .illegal_wr_o     (csr_illegal_wr), // this o/p is valid in the same cycle as the request
             .hpl_o            (pfu_hpl_o)
         );
 endmodule
